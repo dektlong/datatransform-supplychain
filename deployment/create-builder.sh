ec644f53517f1bf1d8fbf7f8a83baee22e4fa9b0
dektlong <dekel.tankel@gmail.com>
2021-05-16 10:21:07 -0700

    Update create-builder.sh

--- deployment/create-builder.sh
+++ deployment/create-builder.sh
@@ -2,6 +2,6 @@
 
 kp builder create onlinestore-builder -n onlinestore-ns \
    --tag goharbor.io/onlinestore/onlinestore-builder \
-   --order onlinestore-builder-order.yaml \
+   --order builder-order.yaml \
    --stack base \
    --store default
